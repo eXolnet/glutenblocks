121fc893d1f8c93c5234c3daa1ee7abf83a184ff
Simon Gaudreau <sgaudreau@exolnet.com>
2019-04-29 11:54:45 -0400

    Update Button block to pass classname on edit and save

--- packages/button/save.js
+++ packages/button/save.js
@@ -1,4 +1,5 @@
 import GenIcon from '../globals/genicon';
+import classnames from 'classnames';
 
 /**
  * BLOCK: Glutenblock Button
@@ -14,7 +15,7 @@ import Ico from '../globals/svgicons';
 
 class GlutenblocksButtonSave extends Component {
     render() {
-        const { attributes: { theme, link, target, noFollow, text, icon, iconSide } } = this.props;
+        const { attributes: { theme, link, target, noFollow, text, icon, iconSide }, className } = this.props;
 
         let relAttr = 'noopener noreferrer';
 
@@ -22,8 +23,13 @@ class GlutenblocksButtonSave extends Component {
             relAttr = relAttr + ' nofollow';
         }
 
+        const classes = classnames({
+            [ `${className}` ]: className,
+            [ `gb-button gb-button--${ theme }` ]: theme,
+        });
+
         return (
-            <a href={ (link ? link : '#') } target={ (target ? target : undefined) } className={`gb-button__btn gb-button__btn--${theme}`} rel={ relAttr }>
+            <a href={ (link ? link : '#') } target={ (target ? target : undefined) } className={classes} rel={ relAttr }>
                 { icon && 'left' === iconSide && (
                     <GenIcon className={ `gb-button__svg-icon gb-button__svg-icon--${ icon } gb-button__svg-icon--${ iconSide }` } name={ icon } icon={ ('fa' === icon.substring(0, 2) ? FaIco[ icon ] : Ico[ icon ]) } />
                 ) }
@@ -37,7 +43,8 @@ class GlutenblocksButtonSave extends Component {
 }
 
 GlutenblocksButtonSave.propTypes = {
-    attributes: PropTypes.object
+    attributes: PropTypes.object,
+    className: PropTypes.string,
 };
 
 export default GlutenblocksButtonSave;
