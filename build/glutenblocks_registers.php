b54bebe155b0e412f604ee686540077c369715d1
Alexandre D'Eschambeault <adeschambeault@exolnet.com>
2019-05-06 11:18:58 -0400

    Update compiled assets

--- build/glutenblocks_registers.php
+++ build/glutenblocks_registers.php
@@ -1,15 +1,15 @@
 <?php
 /**
- * Server-side rendering of the `glutenblocks/jumbotron` block.
+ * Server-side rendering of the `glutenblocks/hero` block.
  * phpcs:disable
  */
 
 /**
- * Registers the `glutenblocks/jumbotron` block on server.
+ * Registers the `glutenblocks/hero` block on server.
  */
-function glutenblocksregister_block_glutenblocks_jumbotron() {
+function glutenblocksregister_block_glutenblocks_hero() {
     register_block_type(
-        'glutenblocks/jumbotron',
+        'glutenblocks/hero',
         [
             'style' => 'glutenblocks-style',
             'editor_script' => 'glutenblocks',
@@ -17,4 +17,4 @@ function glutenblocksregister_block_glutenblocks_jumbotron() {
         ]
     );
 }
-add_action( 'init', 'glutenblocksregister_block_glutenblocks_jumbotron' );
+add_action( 'init', 'glutenblocksregister_block_glutenblocks_hero' );
