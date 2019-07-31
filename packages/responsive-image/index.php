471f1061ff0853d9021b58123f99d387888c3d7a
Simon Gaudreau <sgaudreau@exolnet.com>
2019-07-31 09:30:47 -0400

    Fix linter of gb-responsive-image

--- packages/responsive-image/index.php
+++ packages/responsive-image/index.php
@@ -1,19 +1,22 @@
 <?php
 /**
  * Server-side rendering of the `glutenblocks/responsive-image` block.
+ * phpcs:disable
  */
 
 /**
  * Registers the `glutenblocks/example` block on server.
  */
-function register_block_glutenblocks_responsive_image() {
+function register_block_glutenblocks_responsive_image()
+{
     register_block_type(
         'glutenblocks/responsive-image',
         [
             'style' => 'glutenblocks-style',
             'editor_script' => 'glutenblocks',
-            'editor_style'  => 'glutenblocks-editor',
+            'editor_style' => 'glutenblocks-editor',
         ]
     );
 }
-add_action( 'init', 'register_block_glutenblocks_responsive_image' );
+
+add_action('init', 'register_block_glutenblocks_responsive_image');
