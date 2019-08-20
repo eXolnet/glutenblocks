f62128eff54e3484dfaeda745cd5d4b1cbf8b258
Maxime Mongeau <mmongeau@exolnet.com>
2019-08-20 17:29:04 -0400

    Update the responsive-image save component Refactor to use the new attributes structure Add the old save function to the deprecated block

--- packages/responsive-image/save.js
+++ packages/responsive-image/save.js
@@ -1,88 +1,45 @@
 import PropTypes from 'prop-types';
 
 import { Component, Fragment } from '@wordpress/element';
+import { compose, ifCondition } from '@wordpress/compose';
 
-class GlutenblocksExampleSave extends Component {
+class GlutenblockResponsiveImageSave extends Component {
     render() {
         const {
-            attributes: {
-                desktopImgURL, desktopImgHeight, desktopImgWidth, desktopImgAlt,
-                tabletImgURL, tabletImgHeight, tabletImgWidth, tabletImgAlt,
-                mobileImgURL, mobileImgHeight, mobileImgWidth, mobileImgAlt,
-            },
-            className,
+            attributes, className,
         } = this.props;
 
-        const mobileDisplay = _ => {
-            if (mobileImgURL) {
-                return (
-                    <Fragment>
-                        <figure>
-                            <img src={mobileImgURL}
-                                alt={mobileImgAlt}
-                                width={mobileImgWidth}
-                                height={mobileImgHeight}
-                            />
-                        </figure>
-                    </Fragment>
-                );
-            }
-            return '';
-        };
+        const figure = att => (
+            <Fragment>
+                <figure>
+                    <img src={att.url}
+                        alt={att.alt}
+                        width={att.width}
+                        height={att.height}
+                        style={{ borderRadius: `${att.radius}${att.radiusUnit}` }}
+                    />
+                </figure>
+            </Fragment>
+        );
 
-        const tabletDisplay = _ => {
-            if (tabletImgURL) {
-                return (
-                    <Fragment>
-                        <figure>
-                            <img src={tabletImgURL}
-                                alt={tabletImgAlt}
-                                width={tabletImgWidth}
-                                height={tabletImgHeight}
-                            />
-                        </figure>
-                    </Fragment>
-                );
-            }
-            return '';
-        };
-
-        const desktopDisplay = _ => {
-            if (desktopImgURL) {
-                return (
-                    <Fragment>
-                        <figure>
-                            <img src={desktopImgURL}
-                                alt={desktopImgAlt}
-                                width={desktopImgWidth}
-                                height={desktopImgHeight}
-                            />
-                        </figure>
-                    </Fragment>
-                );
-            }
-            return '';
-        };
+        const imageDisplay = compose(ifCondition(({ url }) => url), figure);
 
         return (
             <div className={className} >
-                <div className="gb-responsive-image gb-responsive-image--desktop">
-                    {desktopDisplay()}
-                </div>
-                <div className="gb-responsive-image gb-responsive-image--tablet">
-                    {tabletDisplay()}
-                </div>
-                <div className="gb-responsive-image gb-responsive-image--mobile">
-                    {mobileDisplay()}
-                </div>
+                {['desktop', 'tablet', 'mobile'].map(size => (
+                    // eslint-disable-next-line react/jsx-key
+                    <div className={`gb-responsive-image gb-responsive-image--${size}`}>
+                        {imageDisplay(attributes[size])}
+                    </div>
+                ))}
             </div>
         );
     }
 }
 
-GlutenblocksExampleSave.propTypes = {
+GlutenblockResponsiveImageSave.propTypes = {
     className: PropTypes.string,
     attributes: PropTypes.object
 };
 
-export default GlutenblocksExampleSave;
+export default GlutenblockResponsiveImageSave;
