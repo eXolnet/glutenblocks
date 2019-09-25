e74abbf8d1d49d4b431ec039137050af8bbe31de
Simon Gaudreau <sgaudreau@exolnet.com>
2019-09-25 09:12:16 -0400

    Make sure the carrouselTimer is compatible with ie11

--- packages/jumbotron-carousel/carouselTimer.js
+++ packages/jumbotron-carousel/carouselTimer.js
@@ -1,26 +1,30 @@
-document.addEventListener('DOMContentLoaded', () => {
+if (window.NodeList && !NodeList.prototype.forEach) {
+    NodeList.prototype.forEach = Array.prototype.forEach;
+}
+
+document.addEventListener('DOMContentLoaded', function () {
     const checkboxes = document.querySelectorAll('.gb-carousel__activator');
     const autoplayDelay = document.getElementById('autoplay-value').value * 1000;
     if (autoplayDelay && autoplayDelay > 0) {
         callTimer();
     }
 
-    document.querySelectorAll('.gb-carousel__manual-next').forEach(element => {
-        element.addEventListener('click', (event) => {
+    document.querySelectorAll('.gb-carousel__manual-next').forEach(function (element) {
+        element.addEventListener('click', function (event) {
             event.preventDefault();
             checkboxes[getNextArrayId()].checked = true;
         });
     });
 
-    document.querySelectorAll('.gb-carousel__manual-prev').forEach(element => {
-        element.addEventListener('click', (event) => {
+    document.querySelectorAll('.gb-carousel__manual-prev').forEach(function (element) {
+        element.addEventListener('click', function (event) {
             event.preventDefault();
             checkboxes[getPrevArrayId()].checked = true;
         });
     });
 
     function callTimer() {
-        setTimeout(_=>{
+        setTimeout(function () {
             checkboxes[getNextArrayId()].checked = true;
             callTimer();
         }, autoplayDelay);
@@ -28,9 +32,16 @@ document.addEventListener('DOMContentLoaded', () => {
 
     function getCurrentArrayId() {
         const currentItem = document.querySelector('.gb-carousel__activator:checked');
-        return Array.from(checkboxes).indexOf(currentItem);
+
+        for (let i = 0; i <= checkboxes.length; i++) {
+            if (checkboxes[i] === currentItem) {
+                return i;
+            }
+        }
+
+        return null;
     }
-    
+
     function getNextArrayId() {
         const currentIndex = getCurrentArrayId();
         return ((currentIndex + 1) > (checkboxes.length - 1)) ? 0 : (currentIndex + 1);
