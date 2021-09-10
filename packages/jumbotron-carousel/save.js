3de6b4e405698b7462dba9274f054825e5143287
Alexandre D'Eschambeault <adeschambeault@exolnet.com>
2021-09-10 12:48:28 -0400

    fix: make sure all loop in jumbotron-carousel have a key attribute

--- packages/jumbotron-carousel/save.js
+++ packages/jumbotron-carousel/save.js
@@ -24,7 +24,7 @@ class GlutenblocksJumbotronCarouselSave extends Jumbotron {
         const elements = [];
         for (let i = 0; i < numberOfSlides; i++) {
             const id = this.getUniqueId() + (i + 1);
-            elements.push(<input className="gb-carousel__activator" type="radio" name="carousel" id={id} checked={i === 0} />);
+            elements.push(<input key={i} className="gb-carousel__activator" type="radio" name="carousel" id={id} checked={i === 0} />);
         }
         return elements;
     }
@@ -36,7 +36,7 @@ class GlutenblocksJumbotronCarouselSave extends Jumbotron {
             const forwardId =  (i + 2 > numberOfSlides) ? (this.getUniqueId() + '1') : this.getUniqueId() + (i + 2);
             const reverseId = (i < 1) ? this.getUniqueId() + numberOfSlides : this.getUniqueId() + i;
             elements.push(
-                <div className="gb-carousel__controls">
+                <div key={i} className="gb-carousel__controls">
                     <label className="gb-carousel__control gb-carousel__control--backward" htmlFor={reverseId}></label>
                     <label className="gb-carousel__control gb-carousel__control--forward" htmlFor={forwardId}></label>
                 </div>
@@ -60,7 +60,7 @@ class GlutenblocksJumbotronCarouselSave extends Jumbotron {
         const { attributes: { numberOfSlides } } = this.props;
         const elements = [];
         for (let i = 0; i < numberOfSlides; i++) {
-            elements.push(<label className="gb-carousel__indicator" htmlFor={this.getUniqueId() + (i + 1)}></label>);
+            elements.push(<label key={i} className="gb-carousel__indicator" htmlFor={this.getUniqueId() + (i + 1)}></label>);
         }
         return elements;
     }
@@ -74,7 +74,7 @@ class GlutenblocksJumbotronCarouselSave extends Jumbotron {
                 <div className="gb-carousel">
                     {super.renderHeroBackgroundOverlay()}
                     <div className="gb-carousel__container">
-                        
+
                         {/* Carousel Navigation Mechanics */}
                         {this.createCarouselActivators()}
 
